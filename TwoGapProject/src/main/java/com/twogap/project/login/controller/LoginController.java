6ba753ee717607d3141acdb98805cbf5f929d7c3
soomin530 <rnldudnstnalsdl@gmail.com>
2024-12-12 14:22:22 +0900

    우수민완료

--- TwoGapProject/src/main/java/com/twogap/project/login/controller/LoginController.java
+++ TwoGapProject/src/main/java/com/twogap/project/login/controller/LoginController.java
@@ -195,7 +195,7 @@ public class LoginController {
 		
 		String message = null;
 		String path = null;
-		
+
 		if(result != null) { // 있는 회원			
 			
 			message = "회원님의 아이디는 " + result.getMemberId() + "입니다.";
@@ -212,6 +212,7 @@ public class LoginController {
 		return "redirect:" + path; 
 
 	}
+
 	
 	
 	
